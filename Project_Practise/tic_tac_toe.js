40166f209e7f90afb2a300ff6bf7aa16fbe3b87b
praveer <praveer.pratap@publicissapient.com>
2022-10-21 18:35:14 +0530

    list element

---
+++ Project_Practise/tic_tac_toe.js
@@ -0,0 +1,14 @@
+btns=document.querySelector(".btn")
+btns.addEventListner("click",function(){
+    console.log("a")
+})
+
+inp=document.querySelector('input')
+imp.addEventListner("click",function(){
+    console.log("b")
+})
+
+// btns.foreach((element)=>{
+//     console.log(element)
+// })
+

336d49e48d76468c7340e583ea59382fdbaa07d2
praveer <praveer.pratap@publicissapient.com>
2022-10-22 23:13:46 +0530

    Section4:Till html semantics

--- Project_Practise/tic_tac_toe.js
+++ Project_Practise/tic_tac_toe.js
@@ -1,14 +1,12 @@
-btns=document.querySelector(".btn")
-btns.addEventListner("click",function(){
-    console.log("a")
+let btns = document.querySelectorAll(".btn")
+console.log(typeof(btns), btns)
+
+btns.forEach(function(btn){
+    btn.addEventListener("click",function(e){
+        console.log("a")
+    })
+    console.log(typeof(btn),btn)
+    
 })
 
-inp=document.querySelector('input')
-imp.addEventListner("click",function(){
-    console.log("b")
-})
-
-// btns.foreach((element)=>{
-//     console.log(element)
-// })
 
